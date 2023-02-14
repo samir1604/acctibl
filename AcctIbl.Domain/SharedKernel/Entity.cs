4a78550b601732ad6912296f043a0db6a4d2b848
samir1604 <samir1604@gmail.com>
2023-02-14 00:13:42 -0500

    Base entity created

--- AcctIbl.Domain/SharedKernel/Entity.cs
+++ AcctIbl.Domain/SharedKernel/Entity.cs
@@ -1,9 +1,45 @@
 namespace AcctIbl.Domain.SharedKernel;
 
-public class Entity<TId> : IEquatable<Entity<TId>>
+public abstract class Entity<TId> : IEquatable<Entity<TId>>
+where TId : notnull
 {
+    protected Entity(TId id)
+    {
+        if (!IsIdValid(id))
+            throw new ArgumentException("Identifier is not a supported format");
+
+        Id = id;
+    }
+
+    public TId Id { get; protected init; }
+
+    public override bool Equals(object? obj)
+    {
+        return Equals(obj as Entity<TId>);
+    }
+
     public bool Equals(Entity<TId>? other)
     {
-        throw new NotImplementedException();
+        return Id?.GetHashCode() == other?.Id?.GetHashCode();
+    }
+
+    public override int GetHashCode()
+    {
+        return Id!.GetHashCode();
+    }
+
+    public static bool operator ==(Entity<TId> left, Entity<TId> right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(Entity<TId> left, Entity<TId> right)
+    {
+        return !(left == right);
+    }
+
+    private static bool IsIdValid(TId id)
+    {
+        return id is int || id is long || id is string || id is Guid;
     }
 }
